f621a7cc8b969f5b1a749cd7738b469eb6116ec3
隆成 平井 <150023@jc-21.jp>
2017-11-24 09:48:20 +0900

    変更3

--- BMI/BMI/MainPage.xaml.cs
+++ BMI/BMI/MainPage.xaml.cs
@@ -13,40 +13,5 @@ namespace BMI
         {
             InitializeComponent();
         }
-
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-            double h = double.Parse(Height.Text);
-            double w = double.Parse(Weight.Text);
-
-            DisplayAlert("身長", h.ToString(), "OK");
-            DisplayAlert("体重", w.ToString(), "OK");
-
-            string s;
-
-            if (h > 3)
-            {
-                h /= 100;
-            }
-
-            if (BMI >= 25)
-            {
-                s = "肥満";
-            }
-
-            else if (BMI > 18.5)
-            {
-                s = "普通体重";
-            }
-
-            else
-            {
-                s = "低体重";
-            }
-            return s;
-
-            double bmi = w / h / h;
-            DisplayAlert("BMI", bmi.ToString(), "OK");
-        }
     }
 }
