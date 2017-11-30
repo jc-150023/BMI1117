e4527edb0797be038034c252f1a2293590409e6c
隆成 平井 <150023@jc-21.jp>
2017-11-30 16:12:34 +0900

    変更4

--- BMI/BMI/MainPage.xaml.cs
+++ BMI/BMI/MainPage.xaml.cs
@@ -15,7 +15,10 @@ namespace BMI
         }
         private void BMI_Button_Click(object sender, EventArgs e)
         {
-            DisplayAlert("BMI", "太りすぎ", "OK");
+            double dHeight = double.Parse(height.Text);
+
+
+            DisplayAlert("BMI", dHeight+":太りすぎ", "OK");
         }
     }
 }
