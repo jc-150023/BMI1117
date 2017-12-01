c0b039086c048bb6276367392a01db3c18c630fb
隆成 平井 <150023@jc-21.jp>
2017-12-01 09:31:54 +0900

    変更8

--- BMI/BMI/MainPage.xaml.cs
+++ BMI/BMI/MainPage.xaml.cs
@@ -16,7 +16,7 @@ namespace BMI
         private void BMI_Button_Click(object sender, EventArgs e)
         {
             double dHeight = double.Parse(height.Text);
-            double dWeight = 50.0;
+            double dWeight = double.Parse(weight.Text);
 
             if (dHeight > 3)
             {
@@ -25,8 +25,16 @@ namespace BMI
 
             double dBMI = dWeight / dHeight / dHeight;
 
+            if (dBMI < 18.5) {
+                DisplayAlert("BMI", dBMI + ":低体重(痩せ型)", "OK");
+            } else if(dBMI < 25.0) {
+                DisplayAlert("BMI", dBMI + ":普通体重", "OK");
+            }
+            else
+            {
+                DisplayAlert("BMI", dBMI + ":肥満", "OK");
+            }
 
-            DisplayAlert("BMI", dHeight+":太りすぎ", "OK");
         }
     }
 }
